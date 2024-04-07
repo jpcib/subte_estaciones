acf420696cb34e8be0903328f0f7873977aade11
jcib-betta <jpcibantos@gmail.com>
2024-04-07 04:08:34 -0300

    Update distancias_estaciones.R
    Replace individual loops for Matrix sf_distances.

--- distancias_estaciones.R
+++ distancias_estaciones.R
@@ -16,36 +16,73 @@ data <- estaciones %>%
   glimpse() %>% 
   print()
 
--Atest[2,4]
+distances <- st_distance(data)
+distances
 
-dt <- st_distance(Atest[1,4],Atest[2,4])
-class(dt)
-as.double(dt)
+max_distances <- max(distances)
+max_distances
 
-distance_df <- data.frame(id = NA,
-                          distancia = NA)
+max_indices <- as.data.frame(which(distances == max_distances, arr.ind = TRUE))[,2]
+max_indices
 
-for (i in 1:nrow(data)) {
-  
-  geom1 <- data$geometry[i]
-  print(geom1)
-  
-  if (i >= nrow(data)) {
-    break
-    
-  } else {
-    
-    geom2 <- data$geometry[i+1]
-    print(geom2)
-    distance <- st_distance(x = geom1, y = geom2)
-    distance_df <- distance_df %>% 
-      add_row(id = i, 
-              distancia = as.double(distance))
-    
-  }
-}
+#Creo necesito la matriz completa, ordenar y extraer los indices de los valores en 
+## diagonal. 
+#Posiblemente esto solo sirva para ordenar, o incluso tenga que reemplazarlo 
+##por dikjstra con las calles para calcular distancias reales. 
+#De todos modos estaria bueno poner lineas en un mapa de este mismo. 
 
-glimpse(distance_df)
+final_distances <- as.data.frame(units::drop_units(distances),row.names = TRUE) %>% 
+  rownames_to_column() %>% 
+  mutate(rowname = as.numeric(rowname)) %>%
+  # # arrange(V14) %>% 
+  # arrange(paste0("V",max_indices[1])) %>%
+  # select(rowname, paste0("V",max_indices[1]), paste0("V",max_indices[2]) ) %>%
+  glimpse() 
+  print()
+
+print(final_distances)
+glimpse(final_distances)
+class(final_distances)
+
+class(distances)
+  select(max_indices[1,2]) %>% 
+  glimpse()
+  arrange(as.integer(max_indices[1,2])) %>% 
+
+
+# dt <- st_distance(Atest[1,4],Atest[2,4])
+# class(dt)
+# as.double(dt)
+# distance_df <- data.frame(id = NA,
+#                           distancia = NA)
+# 
+# for (i in 1:nrow(data)) {
+#   
+#   geom1 <- data$geometry[i]
+#   print(geom1)
+#   id <- data$id[i]
+#   
+#   if (i >= nrow(data)) {
+#     break
+#     
+#   } else {
+#     
+#     geom2 <- data$geometry[i+1]
+#     # print(geom2)
+#     distance <- st_distance(x = geom1, y = geom2)
+#     print(distance)
+#     distance_df <- distance_df %>% 
+#       add_row(id = id, 
+#               distancia = as.double(distance))
+#   }
+# }
+# 
+# 
+# data_final <- inner_join(data, distance_df) %>% 
+#   glimpse()
+# glimpse(distance_df)
+
+###### igraph sofw
 # group_st_distance <- function(data = NULL, geometry = data$geometry, group = NULL) {
 #   for (i in geometry) {
 #     
